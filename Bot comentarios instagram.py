0655c355a53f4f79197622b8112b7def87b9a8cb
Victor E G Oliveira <victorxareu52@gmail.com>
2022-01-19 00:31:39 -0300

    Abertura da pagina inicial do Instagram

--- Bot comentarios instagram.py
+++ Bot comentarios instagram.py
@@ -1,1 +1,18 @@
+from cgi import test
+from selenium import *
+from selenium import webdriver
+
 print("Hello Word")
+
+class InstagramBot:
+    def __init__(self,Username,Password):
+        self.Username = Username
+        self.Password = Password
+        self.driver = webdriver.Chrome(executable_path='D:\Programação\Codigos\Bot comentarios instagram\chromedriver.exe')
+
+    def Login(self):
+        driver = self.driver
+        driver.get('https://www.instagram.com/')
+
+VEGO = InstagramBot('vegotest52','88195104aA')
+VEGO.Login()
