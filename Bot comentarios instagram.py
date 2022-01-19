2710467dd1da1b8263b3a4cb02231d4b899f99fa
Victor E G Oliveira <victorxareu52@gmail.com>
2022-01-19 01:37:43 -0300

    Função login funcionando

--- Bot comentarios instagram.py
+++ Bot comentarios instagram.py
@@ -1,6 +1,9 @@
 from cgi import test
+import time
+from time import *
 from selenium import *
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 print("Hello Word")
 
@@ -13,6 +16,19 @@ class InstagramBot:
     def Login(self):
         driver = self.driver
         driver.get('https://www.instagram.com/')
+        sleep(5)
+        Campo_usuario = driver.find_element_by_name('username')
+        Campo_usuario.click()
+        Campo_usuario.clear()
+        Campo_usuario.send_keys(self.Username)
+        sleep(1)
+        Campo_senha = driver.find_element_by_name('password')
+        Campo_senha.click()
+        Campo_senha.clear()
+        Campo_senha.send_keys(self.Password)
+        sleep(1)
+        Campo_senha.send_keys(Keys.ENTER)
+        sleep(10)
 
 VEGO = InstagramBot('vegotest52','88195104aA')
 VEGO.Login()
